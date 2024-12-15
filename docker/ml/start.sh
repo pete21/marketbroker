751c102612d6fbc2c695eaa0100e973c7805b4c8
pete21 <piotr_n7@yahoo.com>
2024-12-15 02:18:29 +0100

    feat: update docker

--- docker/ml/start.sh
+++ docker/ml/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --add-host=host.docker.internal:host-gateway -p 8888:8888 -p 2000-2010:2000-2010 -v /Users/tzieleniewski/Private/repos/marketbroker2/notebooks/:/opt/notebooks anaconda3_py10_ml:2
+docker run --add-host=host.docker.internal:host-gateway -p 8888:8888 -p 2000-2010:2000-2010 -v $PWD/../../notebooks/:/opt/notebooks anaconda3_py10_ml:2
