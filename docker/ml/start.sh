bc5f1b16f57d4748ec819fa1a20b6e25b781c80c
Piotr Nazarewicz <piotr.nazarewicz@symmetrical.ai>
2026-06-05 21:36:04 +0200

    feat: Dockerfile4 update

--- docker/ml/start.sh
+++ docker/ml/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run --add-host=host.docker.internal:host-gateway -p 8888:8888 -p 2000-2010:2000-2010 -v /Users/tzieleniewski/Private/repos/marketbroker2/notebooks/:/home/jovyan/notebooks -v /Users/tzieleniewski/Private/repos/marketbroker2/ml_examples/:/home/jovyan/examples ml_11:v1.10_cuda-12.9_ubuntu-24.04
+docker run --gpus all --add-host=host.docker.internal:host-gateway -p 8888:8888 -p 2000-2010:2000-2010 -v /Users/tzieleniewski/Private/repos/marketbroker2/notebooks/:/home/jovyan/notebooks -v /Users/tzieleniewski/Private/repos/marketbroker2/ml_examples/:/home/jovyan/examples ml_11:v1.10_cuda-12.9_ubuntu-24.04
 
 
-# docker run -p 8888:8888 -p 2000-2010:2000-2010 -v $(pwd):/home/jovyan/notebooks ml_11:v1.10_cuda-12.9_ubuntu-24.04
+# docker run --gpus all -p 8888:8888 -p 2000-2010:2000-2010 -v $(pwd):/home/jovyan/notebooks ml_11:v1.10_cuda-12.9_ubuntu-24.04
