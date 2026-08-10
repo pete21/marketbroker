40356ccc037044b3436d285cd68bfa898fa3f3fc
Piotr Nazarewicz <piotr.nazarewicz@symmetrical.ai>
2026-08-11 01:57:54 +0200

    Revert "json serialization"
    This reverts commit 17f1d0b1851ac4256c08cd581944665e89dc9546.

--- application/src/main/kotlin/com/piotr/marketbroker/application/service/OrdersService.kt
+++ application/src/main/kotlin/com/piotr/marketbroker/application/service/OrdersService.kt
@@ -31,8 +31,6 @@ import java.time.ZoneOffset
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.round
-import kotlinx.serialization.json.*
-
 
 private const val INSERT_OPEN_ORDER = "InsertOpenOrder"
 private const val GET_OPEN_ORDER = "GetOpenOrder"
@@ -129,8 +127,7 @@ class OrdersService(
 
         val httpResponse: HttpAdapterResponse
         try {
-            // val jsonString = mapper.writeValueAsString(insertClosePositionRequestDTO)
-            val jsonString = Json.encodeToString(insertClosePositionRequestDTO)
+            val jsonString = mapper.writeValueAsString(insertClosePositionRequestDTO)
             log.info("insertClosePosition: $jsonString")
             httpResponse = httpAdapter.postRequest(INSERT_CLOSE_POSITION, jsonString, RequestHeaders.postHeaders)
         } catch (e: Exception) {
@@ -191,8 +188,7 @@ class OrdersService(
 
         var httpResponse: HttpAdapterResponse = HttpAdapterResponse(0, "")
         try {
-            // val jsonString = mapper.writeValueAsString(requestTradeDTO)
-            val jsonString = Json.encodeToString(requestTradeDTO)
+            val jsonString = mapper.writeValueAsString(requestTradeDTO)
             log.info("requestTrade: $jsonString")
             httpResponse = httpAdapter.postRequest(REQUEST_TRADE, jsonString, RequestHeaders.postHeaders)
         } catch (e: Exception) {
@@ -255,8 +251,7 @@ class OrdersService(
 
         var httpResponse: HttpAdapterResponse = HttpAdapterResponse(0, "")
         try {
-            // val jsonString = mapper.writeValueAsString(insertOpenOrderRequestDTO)
-            val jsonString = Json.encodeToString(insertOpenOrderRequestDTO)
+            val jsonString = mapper.writeValueAsString(insertOpenOrderRequestDTO)
             log.info("insertOpenOrder request: $jsonString")
             httpResponse = httpAdapter.postRequest(INSERT_OPEN_ORDER, jsonString, RequestHeaders.postHeaders)
             log.info("insertOpenOrder response: ${httpResponse.statusCode} ${httpResponse.body}")
@@ -486,7 +481,6 @@ internal class RequestTradeDTO (
                 trailingPoint, 0, true, userAgentValue, key)
 }
 
-@Serializable
 internal class InsertOpenOrderRequestDTO(
     private val tradeType: Int = 1,
     private val marketID: Int,
